571cb307f8188e3d4eb508c67aae90540635242b
Hardik <19it.hardikprajapati@adit.ac.in>
2020-11-17 15:53:04 +0530

    Project Done

--- class.js
+++ class.js
@@ -1,5 +1,6 @@
+// Beat class for play corresponding Audio
 class Beat {
-    
+
     constructor(audioSrc) {
         this.audio = new Audio(audioSrc);
     }
@@ -7,5 +8,37 @@ class Beat {
     play = () => {
         this.audio.currentTime = 0;
         this.audio.play();
+    }
+}
+
+// Button class for set up Button Environment
+
+class Button {
+    constructor(color,code){
+        this.color = color;
+        this.code = code;
+        this.element = document.getElementById(code);
+        this.SetBorder();
+        this.AddTransitioendEvent();
+    }
+
+    SetBorder = () =>{
+        this.element.style.borderColor = this.color;
+    }
+
+    SetButton = () =>{
+        this.element.style.backgroundColor = this.color;
+        this.element.style.boxShadow = `0px 0px 17px ${this.color}`;
+    }
+
+    AddTransitioendEvent = () =>{
+        this.element.addEventListener("transitionend",() =>{
+            this.ResetButton();
+        })
+    }
+
+    ResetButton = () =>{
+        this.element.style.backgroundColor = "transparent";
+        this.element.style.boxShadow = 'none';
     }
 }
--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 let beats = {
     "65": {
-        beat: new Beat("./Audio And Background/Piano Chord 331.mp3")
+        beat: new Beat("./Audio And Background/Piano Chord 331.mp3"),
+        button: new Button("#00FFFF",65)
     },
     "83": {
-        beat: new Beat("./Audio And Background/Piano Chord 209.mp3")
+        beat: new Beat("./Audio And Background/Piano Chord 209.mp3"),
+        button: new Button("#00FFFF",83)
     },
     "68": {
-        beat: new Beat("./Audio And Background/Piano Chord 208.mp3")
+        beat: new Beat("./Audio And Background/Piano Chord 208.mp3"),
+        button: new Button("#00FFFF",68)
     },
     "70": {
-        beat: new Beat("./Audio And Background/Drum Sticks Hit 3.mp3")
+        beat: new Beat("./Audio And Background/Drum Sticks Hit 3.mp3"),
+        button: new Button("#FF00FF",70)
     },
     "71": {
-        beat: new Beat("./Audio And Background/Drum Snare Roll.mp3")
+        beat: new Beat("./Audio And Background/Drum Snare Roll.mp3"),
+        button: new Button("#FF00FF",71)
     },
     "72": {
-        beat: new Beat("./Audio And Background/PREL Musical 57.mp3")
+        beat: new Beat("./Audio And Background/PREL Musical 57.mp3"),
+        button: new Button("#FF00FF",72)
     },
     "74": {
-        beat: new Beat("./Audio And Background/Cymbal Suspended 2.mp3")
+        beat: new Beat("./Audio And Background/Cymbal Suspended 2.mp3"),
+        button: new Button("#FF00FF",74)
     },
     "75": {
-        beat: new Beat("./Audio And Background/Musical Compos 33.mp3")
+        beat: new Beat("./Audio And Background/Musical Compos 33.mp3"),
+        button: new Button("#FFFFFF",75)
     },
     "76": {
-        beat: new Beat("./Audio And Background/Musical Orches 4.mp3")
+        beat: new Beat("./Audio And Background/Musical Orches 4.mp3"),
+        button: new Button("#FFFFFF",76)
     }
 };
 
@@ -32,6 +41,7 @@ KeyDownFunction = (event) =>{
     let Code = event.keyCode;
     if(Code in beats){
        beats[Code].beat.play()
+       beats[Code].button.SetButton()
     }
 }
 
